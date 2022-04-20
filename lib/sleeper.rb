7d13a45c06886bae35ec109c7046cd2b56dd53b7
sikandar-saleem-empg <sikandar.ali@empglabs.com>
2022-04-20 13:45:56 +0500

    first commit

---
+++ lib/sleeper.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+require_relative "sleeper/version"
+
+module Sleeper
+  class Error < StandardError; end
+  
+  def sleep()
+    print("sleeping***************************\n")
+  end
+end
+class
+  include Sleeper
+end
---
+++ spec/sleeper_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Sleeper do
+  it "has a version number" do
+    expect(Sleeper::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
